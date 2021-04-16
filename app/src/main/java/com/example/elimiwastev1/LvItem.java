c4aa4419f428df3e817275e2b3afad96f772976e
shravyaanisetti <shravya.anisetti@gmail.com>
2021-04-16 08:18:25 -0400

    Updated fragment__manual.xml

---
+++ app/src/main/java/com/example/elimiwastev1/LvItem.java
@@ -0,0 +1,23 @@
+package com.example.elimiwastev1;
+
+public class LvItem {
+
+    String itemName;
+    String itemDate;
+
+    public String getItemDate() {
+        return itemDate;
+    }
+
+    public void setItemDate(String itemDate) {
+        this.itemDate = itemDate;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+}
---
+++ app/src/main/java/com/example/elimiwastev1/manual_Test.java
@@ -0,0 +1,71 @@
+package com.example.elimiwastev1;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.ListMenuItemView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class manual_Test extends AppCompatActivity {
+    EditText itemName, itemDate;
+    Button save;
+    ListView groceryList;
+    String itemName2, itemDate2;
+    ArrayList<LvItem>arrayList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.manual_test);
+
+
+        itemName = findViewById(R.id.item);
+        itemDate = findViewById(R.id.date);
+        save = findViewById(R.id.save);
+        groceryList = findViewById(R.id.LW);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                itemName2 = itemName.getText().toString();
+                itemDate2 = itemDate.getText().toString();
+
+                LvItem lvItem = new LvItem();
+                lvItem.setItemName(itemName2);
+                lvItem.setItemName(itemDate2);
+                arrayList.add(lvItem);
+
+
+                ContactAdapter contactAdapter = new ContactAdapter(arrayList,manual_Test.this);
+                groceryList.setAdapter(contactAdapter);
+
+
+
+
+
+
+            }
+        });
+
+    }
+
+
+
+
+
+
+
+
+}
+
