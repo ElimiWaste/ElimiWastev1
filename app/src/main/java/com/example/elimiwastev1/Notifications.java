bfba6d7d33960c74a34091c3c972c1261c4a5222
andy0123456789 <andyhueast@gmail.com>
2021-04-26 13:09:46 -0400

    Nothing done, just for commit and push.

---
+++ app/src/main/java/com/example/elimiwastev1/Notifications.java
@@ -0,0 +1,42 @@
+package com.example.elimiwastev1;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+//Nothing here
+public class Notifications extends Application {
+    public static final String CHANNEL_1_ID = "notifications";
+    public static final String CHANNEL_2_ID = "notifications2";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        createNotificationsChannels();
+
+    }
+
+    private void createNotificationsChannels(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel1 = new NotificationChannel(
+                    CHANNEL_1_ID,
+                    "ReminderChannel",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            channel1.setDescription("This is the first channel");
+            //channel. (add default settings to channel)
+            NotificationChannel channel2 = new NotificationChannel(
+                    CHANNEL_2_ID,
+                    "ReminderChannel2",
+                    NotificationManager.IMPORTANCE_LOW
+            );
+            channel2.setDescription("This is the second channel");
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel1);
+            manager.createNotificationChannel(channel2);
+
+        }
+    }
+}
