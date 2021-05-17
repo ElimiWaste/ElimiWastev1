fbd944c02aa65f0f4c864feb2727b595b01d7f2a
Ansh Tripathi <71733324+anshTripathi07@users.noreply.github.com>
2021-05-17 19:51:47 -0400

    Performed a lot of hardcoding strings in strings.xml. Retrying committing

--- app/src/main/java/com/example/elimiwastev1/DeleteNotif.java
+++ app/src/main/java/com/example/elimiwastev1/DeleteNotif.java
@@ -5,7 +5,7 @@ import android.app.NotificationManager;
 import android.content.Context;
 
 import androidx.core.app.NotificationManagerCompat;
-
+//is this needed?
 //https:stackoverflow.com/questions/2665634/how-to-clear-a-notification-in-android#:~:text=Use%20the%20following%20code%20to,cancel(NOTIFICATION_ID)%3B
 public class DeleteNotif {
 }
