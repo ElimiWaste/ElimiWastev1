750f3eeae7b44e132cb237e3002a7579c38e9794
Ansh Tripathi <71733324+anshTripathi07@users.noreply.github.com>
2021-05-17 19:21:13 -0400

    Added comments on Controller.java

--- app/src/main/java/com/example/elimiwastev1/Controller.java
+++ app/src/main/java/com/example/elimiwastev1/Controller.java
@@ -22,25 +22,44 @@ public class Controller extends Application {
     //declares arraylist of Food objects
     ArrayList<Food> foodList = new ArrayList<Food>();
 
+    /**
+     * Getter method for Controller class
+     * @return foodList, the array list filled with all the food objects
+     */
     public ArrayList<Food> getFood() {
         return foodList;
     }
 
+    /**
+     * Adds Food objects to the arraylist
+     * @param yummy, the variable for the incoming food objects into the Food arraylist
+     */
     public void addFood(Food yummy) {
         foodList.add(yummy);
     }
 
+    //Strings to define the two channels for notifications
     public static final String CHANNEL_1_ID = "notifications";
     public static final String CHANNEL_2_ID = "notifications2";
 
-
+    /**
+     * OnCreate method for Controller class
+     * Performs super.onCreate in addition to reading the entire firebase and making an arraylist of all the food objects
+     * Finally creates the notification channels for the application
+     */
     @Override
     public void onCreate() {
         super.onCreate();
-        readQuestionDataFB();
+        readFoodDataFB();
         createNotificationsChannels();
     }
-    private void readQuestionDataFB() {
+
+    /**
+     * Reading the entire firebase directory and storing it locally in an arraylist
+     * Helps to parse through the firebase data and create individual Food objects for all the items
+     * A need to make this more efficient is a future extension
+     */
+    private void readFoodDataFB() {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("Foods");
 
@@ -50,6 +69,9 @@ public class Controller extends Application {
         // Read from the database
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
+            /**
+             * A DataSnapshot is performed and then stored into the arraylist that is previously defined
+             */
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
@@ -62,31 +84,43 @@ public class Controller extends Application {
                 }
             }
 
+            /**
+             * If the data in firebase is not able to be read or parsed, this method is invoked
+             * @param error
+             */
             @Override
             public void onCancelled(DatabaseError error) {
-                // Failed to read value
+                // Failed to read value and logs it into the Logcat
                 Log.w("MainActivity", "Failed to read value.", error.toException());
             }
         });
 
     }
 
+    /**
+     * Method to create the notification channels
+     * One channel aids in the notifications for the half life, while the other notification channel aids in the notifications for the 2 day warning (Importance is set accordingly)
+     */
     private void createNotificationsChannels(){
+        // Creates the NotificationChannel, but only on API 26+ because NotificationChannel is not supported in the previous APIs
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel1 = new NotificationChannel(
                     CHANNEL_1_ID,
                     "ReminderChannel",
+                    //sets priority of the channel to be high
                     NotificationManager.IMPORTANCE_DEFAULT
             );
             channel1.setDescription("This is the first channel");
             NotificationChannel channel2 = new NotificationChannel(
                     CHANNEL_2_ID,
                     "ReminderChannel2",
+                    //sets priority of the second channel to be low
                     NotificationManager.IMPORTANCE_LOW
 
             );
             channel2.setDescription("This is the second channel");
 
+            // Registers the channels of the notifications with the emulator/device. This helps operate the notifications afterwards
             NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(channel1);
             manager.createNotificationChannel(channel2);
